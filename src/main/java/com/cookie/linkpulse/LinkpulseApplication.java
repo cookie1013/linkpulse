aed3a7a9a878981caa64672a745e1ed923c19428
cookie <2364220536@qq.com>
2026-04-21 19:27:24 +0800

    init: initialize spring boot project

---
+++ src/main/java/com/cookie/linkpulse/LinkpulseApplication.java
@@ -0,0 +1,13 @@
+package com.cookie.linkpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkpulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkpulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cookie/linkpulse/LinkpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookie.linkpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkpulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
